d14d48969a4ac4272e39f2460eca86bb5ec70f8d
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-01-23 22:58:58 +0200

    create index.html module

--- src/js/createIndex.js
+++ src/js/createIndex.js
@@ -2,10 +2,7 @@ import searchForm from '../templates/form.hbs';
 // import gallery from '../templates/gallery.hbs';
 // import galleryItems from '../templates/gallery-items.hbs';
 
-const refs = {
-  body: document.body,
-};
 
 // const markupGallery = gallery(galleryItems());
 // console.log(searchForm());
-refs.body.insertAdjacentHTML('afterbegin', searchForm());
+document.body.insertAdjacentHTML('afterbegin', searchForm());
