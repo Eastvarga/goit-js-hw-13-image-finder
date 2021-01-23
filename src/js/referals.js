da5a17ca333e9f293577c8671359f8468e88fefe
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-01-23 23:01:19 +0200

    create refs .js

---
+++ src/js/referals.js
@@ -0,0 +1,8 @@
+const refs = {
+  body: document.body,
+  form: document.getElementById('search-form'),
+  gallery: document.querySelector('.gallery'),
+  button: document.querySelector('.button[data-action="load-more"]'),
+};
+
+export default refs;
