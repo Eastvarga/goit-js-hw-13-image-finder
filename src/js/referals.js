68ad878197759eee0abcad73a84503598b18413d
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-02-07 11:43:04 +0200

    some Correction

--- src/js/referals.js
+++ src/js/referals.js
@@ -1,7 +1,5 @@
 const body = document.body;
 const form = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
-const intersectionObserverTriger = document.querySelector(
-  '.intersection-observer-triger',
-);
-export default { body, form, gallery, intersectionObserverTriger };
+
+export default { body, form, gallery };
